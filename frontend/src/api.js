34b2a0e3082d05cb483f2a119f8cdd535ff36d2c
Par26ival <92543214+Par26ival@users.noreply.github.com>
2025-06-17 15:18:52 +0300

    vercel #4

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',  // This will be relative to the current domain
+    baseURL: '',  // Empty baseURL since we're using relative paths
     withCredentials: true,  // Important for handling cookies/sessions
     headers: {
         'Content-Type': 'application/json'
